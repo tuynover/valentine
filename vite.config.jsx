23a433de1df0c99a84fc72b9688ed58e8756f3fb
Trịnh Tuyền <cobatuoc@gmail..com>
2026-02-13 11:09:18 +0700

    Update source code

--- vite.config.jsx
+++ vite.config.jsx
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Dùng dấu chấm này là "bùa hộ mệnh" để fix lỗi 404
+  base: '/valentine/', // Dùng dấu chấm này là "bùa hộ mệnh" để fix lỗi 404
 })
