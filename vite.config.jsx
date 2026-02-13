7ef7eca2a0d43c4a8775e8b67f043cd685ad50da
Trịnh Tuyền <cobatuoc@gmail..com>
2026-02-13 10:55:03 +0700

    backend

--- vite.config.jsx
+++ vite.config.jsx
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/valentine/', // Quan trọng: Phải có dấu / ở cả đầu và cuối
+  base: './', // Dùng dấu chấm này là an toàn nhất, chấp mọi loại tên Repo
 })
